3860f3366853f25c0fdbaca8bc289a38f96da811
Ken Zhou <kendotzhou@gmail.com>
2021-02-04 22:28:52 -0500

    GOU yes YESSESSSSSSS YEYSSSSSSSS YESSS

--- js/app/fetch.js
+++ js/app/fetch.js
@@ -25,24 +25,42 @@ define(["app/auth", "app/db", "app/storage", "app/el", "app/helper/util"], funct
                     var img = document.createElement("img");
                     img.classList.add("shared-image");
                     img.setAttribute("src", url);
-                    img.setAttribute("title", "Click to copy")
+                    img.setAttribute("title", "Click to copy");
+                    img.setAttribute("crossorigin", "anonymous");
                     img.addEventListener('click', function () {
-                        var xhr = new XMLHttpRequest();
+                        /*var xhr = new XMLHttpRequest();
                         xhr.responseType = 'blob';
                         xhr.onload = (event) => {
                             var blob = xhr.response;
+                            var i = {};
+                            i["blob.type"] = blob;
                             try {
                                 navigator.clipboard.write([
-                                    new ClipboardItem({
-                                        'image/png': blob
-                                    })
+                                    new ClipboardItem(i)
                                 ]);
                             } catch (error) {
                                 console.error(error);
                             }
                         };
                         xhr.open('GET', url);
-                        xhr.send();
+                        xhr.send();*/
+
+                        var c = document.createElement('canvas');
+                        var ctx = c.getContext('2d');
+                        c.width = this.naturalWidth;
+                        c.height = this.naturalHeight;
+                        ctx.drawImage(this, 0, 0);
+                        var imgBlob;
+                        c.toBlob((blob)=>{
+                            try {
+                                navigator.clipboard.write([
+                                    new ClipboardItem({"image/png": blob})
+                                ]);
+                            } catch (error) {
+                                console.error(error);
+                            }
+                        });
+                        
                     });
 
                     imgsToBeAdded.push(img);
